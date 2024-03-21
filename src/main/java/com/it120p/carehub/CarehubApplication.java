6c35495a71ecb53f90183edd251afd55e0bc9863
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-03-21 16:22:55 +0800

    Springboot Implementation

---
+++ src/main/java/com/it120p/carehub/CarehubApplication.java
@@ -0,0 +1,13 @@
+package com.it120p.carehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/it120p/carehub/CarehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it120p.carehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
