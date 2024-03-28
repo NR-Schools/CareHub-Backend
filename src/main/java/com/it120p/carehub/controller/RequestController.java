bbde5ecf0b19a52c66944a4a1b306cb8971b16b9
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-03-28 20:52:03 +0800

    Add Implementation for create customer request

--- src/main/java/com/it120p/carehub/controller/RequestController.java
+++ src/main/java/com/it120p/carehub/controller/RequestController.java
@@ -1,9 +1,14 @@
 package com.it120p.carehub.controller;
 
 import com.it120p.carehub.model.entity.Request;
+import com.it120p.carehub.model.entity.Status;
+import com.it120p.carehub.model.entity.User;
 import com.it120p.carehub.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -11,4 +16,47 @@ public class RequestController {
 
     @Autowired
     private RequestService requestService;
+
+    @PostMapping("")
+    public Request createCustomerRequest(
+            Authentication authentication,
+            @RequestParam("requestDetails") String requestDetails
+    ) {
+        // Get User from Authentication
+        User user = (User) authentication.getPrincipal();
+
+        // Create new Request
+        Request newRequest = Request.builder()
+                .requestDetails(requestDetails)
+                .requestStatus(Status.PENDING)
+                .customer(user)
+                .build();
+
+        // Save Request
+        return requestService.saveRequest(newRequest);
+    }
+
+    @GetMapping("")
+    public List<Request> getCustomerRequests(
+            Authentication authentication
+    ) {
+        return List.of();
+    }
+
+    @PutMapping("")
+    public Request updateCustomerRequest(
+            Authentication authentication,
+            @RequestParam("requestId") long requestId,
+            @RequestParam("requestDetails") String requestDetails
+    ) {
+        return null;
+    }
+
+    @DeleteMapping("")
+    public Request deleteCustomerRequest(
+            Authentication authentication,
+            @RequestParam("requestId") long requestId
+    ) {
+        return null;
+    }
 }
--- src/main/java/com/it120p/carehub/service/RequestService.java
+++ src/main/java/com/it120p/carehub/service/RequestService.java
@@ -1,5 +1,6 @@
 package com.it120p.carehub.service;
 
+import com.it120p.carehub.model.entity.Request;
 import com.it120p.carehub.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class RequestService {
 
     @Autowired
     private RequestRepository requestRepository;
+
+    public Request saveRequest(Request request) {
+        return requestRepository.save(request);
+    }
 }
