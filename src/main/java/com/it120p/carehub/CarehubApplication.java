de8f063ee0abeebb618f0320b467866d2e29ea5a
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-04-19 09:14:53 +0800

    Make email operation asynchronous

--- src/main/java/com/it120p/carehub/CarehubApplication.java
+++ src/main/java/com/it120p/carehub/CarehubApplication.java
@@ -3,9 +3,11 @@ package com.it120p.carehub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class CarehubApplication {
 
 	public static void main(String[] args) {
