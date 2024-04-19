7d6833a1a5ebe78c0804674b16360ef0700ee08a
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-04-19 10:01:04 +0800

    Remove twilio

--- src/main/java/com/it120p/carehub/CarehubApplication.java
+++ src/main/java/com/it120p/carehub/CarehubApplication.java
@@ -1,10 +1,5 @@
 package com.it120p.carehub;
 
-import com.it120p.carehub.config.TwilioConfig;
-import com.twilio.Twilio;
-import jakarta.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
